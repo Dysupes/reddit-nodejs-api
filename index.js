bb8e17bf2ce7413f2802e025b056cd29b35f5d63
Dylan Pelletier <dysupes79@gmail.com>
2016-08-15 15:23:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root', // CHANGE THIS :)
-  password : 'Sayuri97',
+  password : '',
   database: 'reddit'
 });
 
